7d2638a341e4573d6f06370d64eb2b97473ab390
kaustav Das <kstvds61@gmail.com>
2018-01-12 19:45:43 +0530

    Basic

--- src/test/java/ObjectRepository/LoginPage.java
+++ src/test/java/ObjectRepository/LoginPage.java
@@ -3,10 +3,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class LoginPage {
-    
+	
 	public static final By LoginId = By.xpath("//*[@id='userID']");
-    public static final By pwd = By.name("//*[@id='password']");
+    public static final By password = By.xpath("//*[@id='password']");
     public static final By Companycode = By.xpath("//*[@id='companyCode']");
     public static final By Submit = By.xpath("//*[@id='button']");
-
+    public static final By Closetuto = By.xpath("/html/body/div[14]");
 }
